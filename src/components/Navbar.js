33499f539705db48ce3ca9e3b293e5daeebcb0f6
Mahir Kumar <mahir.kumar@s.amity.edu>
2025-08-29 15:18:11 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ export default function App() {
     const dispatch = useDispatch();
     useEffect(() => {
       dispatch(getCartTotal())
-    }, [cart])
+    }, [dispatch])
     
 
     return (
